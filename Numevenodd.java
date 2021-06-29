8c9decc6eefa980c611139d6c9632883ac21062d
molli-sunanda <71446396+molli-sunanda@users.noreply.github.com>
2021-06-29 13:21:16 +0530

    Add files via upload

---
+++ Numevenodd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Numevenodd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int e=0,o=0;
+while(n>0)
+{
+int i=n%10;
+if(i%2==0)
+e=e+1;
+else
+o=o+1;
+n=n/10;
+}
+System.out.println("Number of Even numbers="+e);
+System.out.println("Number of Odd numbers="+o);
+}
+}
